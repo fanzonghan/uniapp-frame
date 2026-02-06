7477593e86e62332b60b02be21f8f138cfb6d035
fanzonghan <fanzh610@163.com>
2026-02-07 00:05:19 +0800

    引入firstui

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
-Vue.config.productionTip = false
+import fui from './common/fui-app'
 
+Vue.config.productionTip = false
+Vue.prototype.fui = fui
 App.mpType = 'app'
 
 const app = new Vue({
